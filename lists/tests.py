90c72a774598a80f2fff07462bc660d5ccb7e075
KLee <kevinlee05@gmail.com>
2017-07-24 10:54:33 +0000

    refactor homepage view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -16,12 +17,25 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+        #tested implicitly by the django test client
 
-    def test_home_page_returns_corrent_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+    def test_home_page_returns_correct_html(self):
+        #request = HttpRequest()
+        #response = home_page(request)
+        response = self.client.get('/')
+        
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
+
+#    def test_home_page_returns_correct_html(self):
+#        request = HttpRequest()
+#        response = home_page(request)
+#        html = response.content.decode('utf8')
+#        expected_html = render_to_string('home.html')
+#        self.assertEqual(html, expected_html)
+
 
