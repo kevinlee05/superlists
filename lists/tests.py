5e21b8bff28c1a2ba7c4c99ae42c3252da252178
KLee <kevinlee05@gmail.com>
2017-07-25 08:29:16 +0000

    some more tests

--- lists/tests.py
+++ lists/tests.py
@@ -14,28 +14,29 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+#    def test_root_url_resolves_to_home_page_view(self):
+#        found = resolve('/')
+#        self.assertEqual(found.func, home_page)
         #tested implicitly by the django test client
 
-    def test_home_page_returns_correct_html(self):
-        #request = HttpRequest()
-        #response = home_page(request)
-        response = self.client.get('/')
-        
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+#    def test_home_page_returns_correct_html(self):
+#        response = self.client.get('/')
+#
+#        html = response.content.decode('utf8')
+#        self.assertTrue(html.startswith('<html>'))
+#        self.assertIn('<title>To-Do lists</title>', html)
+#        self.assertTrue(html.strip().endswith('</html>'))
 
+ #       self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-#    def test_home_page_returns_correct_html(self):
-#        request = HttpRequest()
-#        response = home_page(request)
-#        html = response.content.decode('utf8')
-#        expected_html = render_to_string('home.html')
-#        self.assertEqual(html, expected_html)
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
 
 
