a7147e74a9cd51f369c82dc2ab0e1f0967a4746a
KLee <kevinlee05@gmail.com>
2017-07-25 10:18:40 +0000

    refactored FT added checkforrowinlisttable function

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #user has heard about new online todo app.
         #user goes to homepage
@@ -34,14 +39,15 @@ class NewVisitorTest(unittest.TestCase):
         # when user hits enter, page updates and page lists the item in the todo list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        #rows = table.find_elements_by_tag_name('tr')
         #self.assertTrue(
         #        any(row.text == '1: Buy peacock feathers' for row in rows),
         #        f"New to-do item did not appear in table. Contents were:\n{table.text}"
         #    )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # there is still a textbox inviting her to add another item. user enters
         # "use peacock feathers to make a fly"
@@ -51,19 +57,19 @@ class NewVisitorTest(unittest.TestCase):
         timep.sleep(1)
 
         #page updates and shows the second item on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
         self.fail('Finish the test!')
 
-# page updates and shows both items on list
+        # site has generated a unique URL for user with some explanatory text
 
-# site has generated a uniqur URL for user with some explanatory text
-
-# user visits the URL
+        # user visits the URL
 
 # user quits
 
