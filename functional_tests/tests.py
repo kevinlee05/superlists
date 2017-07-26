7f8f729b16b690e0d21d5ad10dacca100c76fb40
KLee <kevinlee05@gmail.com>
2017-07-26 05:31:40 +0000

    make functional_tests an app, use Liverservertestcase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +21,8 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #user has heard about new online todo app.
         #user goes to homepage
-        self.browser.get('http://klee05.pythonanywhere.com')
+        #self.browser.get('http://klee05.pythonanywhere.com')
+        self.browser.get(self.live_server_url)
 
         #user notices the page title and header mention todo-lists
         self.assertIn('To-Do', self.browser.title)
@@ -42,25 +44,16 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(
-        #        any(row.text == '1: Buy peacock feathers' for row in rows),
-        #        f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        #    )
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
 
         # there is still a textbox inviting her to add another item. user enters
         # "use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        timep.sleep(1)
+        time.sleep(1)
 
         #page updates and shows the second item on the list
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
