1ed62ec4f5b17b7d7b77e3eec287825a013f95ae
KLee <kevinlee05@gmail.com>
2018-02-15 05:06:03 +0000

    refactor urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
+    #  - refactored list urls
+    # url(r'^lists/new$', views.new_list, name='new_list'),
+    # url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    # url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     url(r'^admin/', admin.site.urls),
 
 ]
